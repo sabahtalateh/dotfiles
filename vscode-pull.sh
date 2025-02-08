24ad182cb6170aa35352488d55492098abe05769
sabahtalateh <sabahtalateh@gmail.com>
2025-02-09 01:27:58 +0300

    w

--- vscode-pull.sh
+++ vscode-pull.sh
@@ -6,6 +6,7 @@ fi
 
 SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 SETTINGS_FILE="${SETTINGS_DIR}/settings.json"
+KEYBINDINGS_FILE="${SETTINGS_DIR}/keybindings.json"
 SNIPPETS_DIR="${SETTINGS_DIR}/snippets"
 
 if [ ! -e "${SETTINGS_FILE}" ]; then
@@ -13,6 +14,13 @@ if [ ! -e "${SETTINGS_FILE}" ]; then
 else
     echo "Copy existing VSCode settings"
     cp "${SETTINGS_FILE}" vscode/settings.json
+fi
+
+if [ ! -e "${KEYBINDINGS_FILE}" ]; then
+    echo "No existing VSCode keybindings found. Skip step"
+else
+    echo "Copy existing VSCode keybindings"
+    cp "${SETTINGS_FILE}" vscode/keybindings.json
 fi
 
 if [ ! -d "${SNIPPETS_DIR}" ]; then 
--- vscode.sh
+++ vscode.sh
@@ -7,6 +7,8 @@ fi
 SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 SETTINGS_FILE="${SETTINGS_DIR}/settings.json"
 SETTINGS_BKP_FILE="${SETTINGS_DIR}/settings.json.bkp"
+KEYBINDINGS_FILE="${SETTINGS_DIR}/keybindings.json"
+KEYBINDINGS_BKP_FILE="${SETTINGS_DIR}/keybindings.json.bkp"
 
 echo "Copy VSCode settings"
 if [ -e "${SETTINGS_FILE}" ]; then
@@ -14,6 +16,13 @@ if [ -e "${SETTINGS_FILE}" ]; then
     cp "${SETTINGS_FILE}" "${SETTINGS_BKP_FILE}"
 fi
 cp vscode/settings.json "${SETTINGS_FILE}"
+
+echo "Copy VSCode keybindings"
+if [ -e "${KEYBINDINGS_FILE}" ]; then
+    echo "Copy existing settings to ${KEYBINDINGS_BKP_FILE}"
+    cp "${KEYBINDINGS_FILE}" "${KEYBINDINGS_BKP_FILE}"
+fi
+cp vscode/keybindings.json "${KEYBINDINGS_FILE}"
 
 echo "Copy VSCode snippets"
 mkdir -p "${SNIPPETS_DIR}" 2> /dev/null

9466dc8394b1e957dea0b9709bbb87e9aa0ba668
sabahtalateh <sabahtalateh@gmail.com>
2025-02-09 01:14:19 +0300

    w

--- vscode-pull.sh
+++ vscode-pull.sh
@@ -8,11 +8,17 @@ SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 SETTINGS_FILE="${SETTINGS_DIR}/settings.json"
 SNIPPETS_DIR="${SETTINGS_DIR}/snippets"
 
-
 if [ ! -e "${SETTINGS_FILE}" ]; then
     echo "No existing VSCode settings found. Skip step"
 else
     echo "Copy existing VSCode settings"
     cp "${SETTINGS_FILE}" vscode/settings.json
+fi
+
+if [ ! -d "${SNIPPETS_DIR}" ]; then 
+    echo "No existing VSCode snippets found. Skip step"
+else
+    echo "Copy existing VSCode snippets"
+    cp -r "${SNIPPETS_DIR}/." vscode/snippets/
 fi
 
--- vscode.sh
+++ vscode.sh
@@ -7,20 +7,17 @@ fi
 SETTINGS_DIR="${HOME}/Library/Application Support/Code/User"
 SETTINGS_FILE="${SETTINGS_DIR}/settings.json"
 SETTINGS_BKP_FILE="${SETTINGS_DIR}/settings.json.bkp"
-SNIPPETS_DIR="${SETTINGS_DIR}/snippets"
 
-echo "Link VSCode settings.json"
+echo "Copy VSCode settings"
 if [ -e "${SETTINGS_FILE}" ]; then
     echo "Copy existing settings to ${SETTINGS_BKP_FILE}"
     cp "${SETTINGS_FILE}" "${SETTINGS_BKP_FILE}"
 fi
-
-echo "Copy settings.json"
 cp vscode/settings.json "${SETTINGS_FILE}"
 
 echo "Copy VSCode snippets"
-mkdir "${SNIPPETS_DIR}" 2> /dev/null
-cp -r vscode/snippets/* "${SNIPPETS_DIR}"
+mkdir -p "${SNIPPETS_DIR}" 2> /dev/null
+cp -r vscode/snippets/. "${SETTINGS_DIR}/snippets/"
 
 echo "Install VSCode extensions"
 cat vscode/extensions.txt | xargs -L 1 code --force --install-extension

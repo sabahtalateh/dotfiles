de265d5c9775874e161e951b0aad0e46ab390646
sabahtalateh <sabahtalateh@gmail.com>
2025-02-09 03:06:00 +0300

    w

--- karabiner.sh
+++ karabiner.sh
@@ -1,2 +1,8 @@
-echo "Link karabiner config"
-ln -s karabiner/karabiner.json ~/.config/karabiner/karabiner.json
+CONFIG_FILE="${HOME}/.config/karabiner/karabiner.json"
+
+echo "Copy karabiner config"
+if [ -e "${CONFIG_FILE}" ]; then
+    echo "Copy existing config to ${SETTINGS_BKP_FILE}"
+    cp "${CONFIG_FILE}" "${CONFIG_FILE_BKP}"
+fi
+cp karabiner/karabiner.json "${CONFIG_FILE}"

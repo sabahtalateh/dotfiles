10807d9677371d0221ab7329fced86004ae54d11
sabahtalateh <sabahtalateh@gmail.com>
2025-02-14 02:24:53 +0300

    w

---
+++ omz-plugins.sh
@@ -0,0 +1,5 @@
+echo "Install zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Install zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting

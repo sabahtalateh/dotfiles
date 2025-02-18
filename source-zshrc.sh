b711265024c98a0b1d262cd3446a8e6798347898
sabahtalateh <sabahtalateh@gmail.com>
2025-02-18 13:36:29 +0300

    w

--- source-zshrc.sh
+++ source-zshrc.sh
@@ -1,14 +1,15 @@
 ZSHRC_FILE="${HOME}/.zshrc"
-SOURCE_STRING="source \${HOME}/.dotfiles/zsh/zshrc"
+SOURCE_FILE="\${HOME}/.dotfiles/zsh/zshrc"
+SOURCE_STRING="source ${SOURCE_FILE}"
 
 if grep -q "${SOURCE_STRING}" "${ZSHRC_FILE}"; then
-    echo "\${HOME}/.dotfiles/zsh/zshrc already sourced"
+    echo "${SOURCE_FILE} already sourced within ${ZSHRC_FILE}"
 else
-    echo "Add \${HOME}/.dotfiles/zsh/zshrc sourcing to ${ZSHRC_FILE}"
+    echo "Add ${SOURCE_FILE} sourcing to ${ZSHRC_FILE}"
 
     echo "\n" >> "${ZSHRC_FILE}"
-    echo "# Added from ${HOME}/.dotfiles/zshrc.sh" >> "${ZSHRC_FILE}"
+    echo "# Added from \${HOME}/.dotfiles/zshrc.sh" >> "${ZSHRC_FILE}"
     echo "${SOURCE_STRING}" >> "${ZSHRC_FILE}"
-fi
 
-echo "To take effect restart terminal or execute: source ${HOME}/.zshrc"
+    echo "To take effect restart terminal or execute: source ${HOME}/.zshrc"
+fi
